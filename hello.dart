f63606f51e4ee17a2b6b37648078f20f5122781b
Sizwe Nxele <sizwenduduzow@gmail.com>
2021-01-21 04:55:46 +0200

    added more dart types

--- hello.dart
+++ hello.dart
@@ -1,3 +1,48 @@
 main() {
-  print("I love dart and flutter");
+  hello obj = new hello(
+      "Sizwe", "Nxele", 34000, 26, true, "9407085624086", "Nxamalala");
+  obj.display();
+}
+
+class hello {
+  // dart data types
+  String name = "";
+  String surname = "";
+  double salary = 0.0;
+  int age = 0;
+  bool genger = false;
+  String gender_status = "";
+  dynamic id;
+  var address;
+
+  hello(String name, String surname, double wage, int age, bool genger,
+      dynamic id, String address) {
+    this.name = name;
+    this.surname = surname;
+    this.salary = wage;
+    this.age = age;
+    this.genger = genger;
+    this.id = id;
+    this.address = address;
+  }
+
+  getGender() {
+    if (this.genger != true) {
+      this.gender_status = "Female";
+    } else {
+      this.gender_status = "Male";
+    }
+    return this.gender_status;
+  }
+
+  display() {
+    print('''
+    name : $name
+    surname : $surname
+    age : $age
+    salary : R$salary
+    gender : ${getGender()}
+    ID no : $id
+    address : $address''');
+  }
 }

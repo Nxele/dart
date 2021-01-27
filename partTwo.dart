da7d2cef0a3bfd07e25d13e7e2527ef885816353
DevSizwe <sizwenduduzow@gmail.com>
2021-01-27 17:41:03 +0200

    added inner loop also known as nasted loop

--- partTwo.dart
+++ partTwo.dart
@@ -4,8 +4,21 @@ main(){
 }
 class partTwo{
     looping(){
+      outerLoop: //this label represent constrol of the outer loop
+
       for(var i = 0; i < 5; i++){
-        print("Inner loop: ${i}");
+        print("Outer loop: ${i}");
+        innerLoop: //this label represent the constrol of inner loop
+
+        for(var j = 0; j < 5 ; j++){
+          if(j>3) break;
+          
+          if(i==2) break innerLoop;
+
+          if(i==4) break outerLoop;
+
+          print("Inner Loop: ${j}");
+        }
       }
     }
 }

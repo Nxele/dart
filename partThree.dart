c069ae9113d03be19288a00206931d93f0b22fb2
DevSizwe <sizwenduduzow@gmail.com>
2021-02-12 00:21:56 +0200

    added list important properties

--- partThree.dart
+++ partThree.dart
@@ -1,6 +1,7 @@
 main(){
   partThree obj = new partThree();
   obj.fixedList();
+  obj.growableList();
 }
 class partThree{
   fixedList(){
@@ -13,5 +14,34 @@ class partThree{
     list_name[4] = "Bongani";
 
     print(list_name);
+  }
+
+  growableList(){
+    //not fixed list
+    var list_cars = ["BMW","Mercedes","Porcshe","Honda"];
+    var list_girls = new List();
+
+    //adding values to the list
+    list_cars.add("Jeep");
+    list_cars.add("Mclaren");
+
+    //remove a value from a list
+    list_cars.remove("Honda");
+    print(list_cars);
+
+
+    //List properties 
+    print(list_cars.first);
+
+    //Other list properties
+    print('''
+    first
+    isEmpty
+    isNotEmpty
+    lenght
+    last
+    reversed
+    single
+    ''');
   }
 }

c3c361621e9a8e87acb9e1bdcc639d0236630450
DevSizwe <sizwenduduzow@gmail.com>
2021-02-12 00:06:26 +0200

    initializing data to a fixed list

--- partThree.dart
+++ partThree.dart
@@ -4,6 +4,14 @@ main(){
 }
 class partThree{
   fixedList(){
+    //fixed lenght list
     var list_name = new List(5);
+    list_name[0] = "Sizwe";
+    list_name[1] = "Mxo";
+    list_name[2] = "Mino";
+    list_name[3] = "Sisa";
+    list_name[4] = "Bongani";
+
+    print(list_name);
   }
 }
